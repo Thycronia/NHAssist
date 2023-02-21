811edb863ed09caede04a48b7c622f4490aa918e
Thycronia <74588623+Thycronia@users.noreply.github.com>
2023-02-21 15:58:32 +0000

    Update Calamity buff namespace for 2.0.2.1

--- UIs/ThyPanel/ContentPanels/OptionPanel/Buff/Calamity.cs
+++ UIs/ThyPanel/ContentPanels/OptionPanel/Buff/Calamity.cs
@@ -18,18 +18,18 @@ namespace NHAssist.UIs.ThyPanel.ContentPanels.OptionPanel.Buff
                 (ModContent.BuffType<Cal.Placeables.CorruptionEffigyBuff>(), -1),
                 (ModContent.BuffType<Cal.Placeables.CrimsonEffigyBuff>(), -1),
                 (ModContent.BuffType<Cal.Placeables.EffigyOfDecayBuff>(), FloatStages.KingSlime),
-                (ModContent.BuffType<Cal.Potions.TriumphBuff>(), BossLib.PostBossStage(ModContent.NPCType<CalNPC.DesertScourge.DesertScourgeHead>())),
+                //(ModContent.BuffType<Cal.Potions.TriumphBuff>(), BossLib.PostBossStage(ModContent.NPCType<CalNPC.DesertScourge.DesertScourgeHead>())),
 
-                (ModContent.BuffType<Cal.Potions.YharimPower>(), FloatStages.Skeletron),
+                //(ModContent.BuffType<Cal.Potions.YharimPower>(), FloatStages.Skeletron),
                 (ModContent.BuffType<Cal.Potions.TeslaBuff>(), BossLib.PostBossStage(ModContent.NPCType<CalNPC.HiveMind.HiveMind>())),
                 (ModContent.BuffType<Cal.Potions.Omniscience>(), FloatStages.Skeletron),
                  (ModContent.BuffType<Cal.Potions.ShadowBuff>(), FloatStages.Skeletron),
 
-                (ModContent.BuffType<Cal.Potions.CadancesGrace>(), FloatStages.WOF),
+                //(ModContent.BuffType<Cal.Potions.CadancesGrace>(), FloatStages.WOF),
                 (ModContent.BuffType<Cal.Potions.PhotosynthesisBuff>(), FloatStages.WOF),
                 //(ModContent.BuffType<Cal.Potions.Revivify>(), FloatStages.WOF),
                 (ModContent.BuffType<Cal.Potions.Soaring>(), FloatStages.WOF),
-                (ModContent.BuffType<Cal.Potions.ArmorCrumbling>(), FloatStages.WOF),
+                (ModContent.BuffType<Cal.Potions.WeaponImbueCrumbling>(), FloatStages.WOF),
 
                 (ModContent.BuffType<Cal.Alcohol.WhiskeyBuff>(), FloatStages.WOF),
                 (ModContent.BuffType<Cal.Alcohol.RumBuff>(), FloatStages.WOF),
@@ -47,9 +47,9 @@ namespace NHAssist.UIs.ThyPanel.ContentPanels.OptionPanel.Buff
                 (ModContent.BuffType<Cal.Alcohol.ScrewdriverBuff>(), FloatStages.MechBoss),
                 (ModContent.BuffType<Cal.Alcohol.WhiteWineBuff>(), FloatStages.MechBoss),
 
-                (ModContent.BuffType<Cal.Potions.AbyssalWeapon>(), BossLib.PostBossStage(ModContent.NPCType<CalNPC.Calamitas.CalamitasClone>())),
+                (ModContent.BuffType<Cal.Potions.WeaponImbueBrimstone>(), BossLib.PostBossStage(ModContent.NPCType<CalNPC.CalClone.CalamitasClone>())),
 
-                (ModContent.BuffType<Cal.Potions.PenumbraBuff>(), FloatStages.Plantera),
+                //(ModContent.BuffType<Cal.Potions.PenumbraBuff>(), FloatStages.Plantera),
                 (ModContent.BuffType<Cal.Alcohol.MargaritaBuff>(), FloatStages.Plantera),
                 (ModContent.BuffType<Cal.Alcohol.EvergreenGinBuff>(), FloatStages.Plantera),
                 (ModContent.BuffType<Cal.Alcohol.CaribbeanRumBuff>(), FloatStages.Plantera),
@@ -60,18 +60,18 @@ namespace NHAssist.UIs.ThyPanel.ContentPanels.OptionPanel.Buff
                 (ModContent.BuffType<Cal.Alcohol.StarBeamRyeBuff>(), BossLib.PostBossStage(ModContent.NPCType<CalNPC.AstrumAureus.AstrumAureus>())),
 
                 //(ModContent.BuffType<Cal.Potions.ArmorCrumbling>(), FloatStages.Golem),
-                (ModContent.BuffType<Cal.Potions.TitanScale>(), FloatStages.Golem),
+                //(ModContent.BuffType<Cal.Potions.TitanScale>(), FloatStages.Golem),
                 (ModContent.BuffType<Cal.Alcohol.MoonshineBuff>(), FloatStages.Golem),
                 (ModContent.BuffType<Cal.Alcohol.MoscowMuleBuff>(), FloatStages.Golem),
                 (ModContent.BuffType<Cal.Alcohol.CinnamonRollBuff>(), FloatStages.Golem),
                 (ModContent.BuffType<Cal.Alcohol.TequilaSunriseBuff>(), FloatStages.Golem),
 
-                (ModContent.BuffType<Cal.Potions.ProfanedRageBuff>(), FloatStages.MoonLord),
-                (ModContent.BuffType<Cal.Potions.HolyWrathBuff>(), FloatStages.MoonLord),
+                //(ModContent.BuffType<Cal.Potions.ProfanedRageBuff>(), FloatStages.MoonLord),
+                (ModContent.BuffType<Cal.Potions.WeaponImbueHolyFlames>(), FloatStages.MoonLord),
 
                 (ModContent.BuffType<Cal.Potions.CeaselessHunger>(), FloatStages.Sentinel),
 
-                (ModContent.BuffType<Cal.Potions.DraconicSurgeBuff>(), FloatStages.Yharon),
+                //(ModContent.BuffType<Cal.Potions.DraconicSurgeBuff>(), FloatStages.Yharon),
             };
             return result;
         }
